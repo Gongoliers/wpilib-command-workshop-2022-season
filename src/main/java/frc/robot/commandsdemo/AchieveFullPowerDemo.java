b5a30b5840f629f48cb6f7198b194992c096506f
Kyle Corry <kylecorry31@gmail.com>
2021-12-12 16:50:57 -0500

    Add withtimeout example

--- src/main/java/frc/robot/commandsdemo/AchieveFullPowerDemo.java
+++ src/main/java/frc/robot/commandsdemo/AchieveFullPowerDemo.java
@@ -5,10 +5,10 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class AchieveFullPowerDemo extends SequentialCommandGroup {
 
     public AchieveFullPowerDemo(){
-        addCommands(
+        super(
             new DriveToPowerWheelDemo(),
             new ExtendSpinnerDemo(),
-            new SpinToFullPowerDemo(),
+            new SpinToFullPowerDemo().withTimeout(3),
             new RetractSpinnerDemo()
         );
     }
